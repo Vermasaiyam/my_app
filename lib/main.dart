774c186ce7a3ae8e5c668119e375850c31a43edb
Vermasaiyam <vermasaiyam9@gmail.com>
2025-07-07 10:30:45 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -38,14 +38,74 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          color: Colors.orange,
-          child: Text("Hello ji"),
-        ),
-      )
+
+      body: ,
+
+
+      // body: Container(
+      //   width: 600,
+      //   height: 300,
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //     crossAxisAlignment: CrossAxisAlignment.stretch,
+      //     children: [
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //         crossAxisAlignment: CrossAxisAlignment.start,
+      //         children: [
+      //           Text('A', style: TextStyle(fontSize: 25),),
+      //           Text('A', style: TextStyle(fontSize: 25),),
+      //           Text('A', style: TextStyle(fontSize: 25),),
+      //           Text('A', style: TextStyle(fontSize: 25),),
+      //           Text('A', style: TextStyle(fontSize: 25),),
+      //         ],
+      //       ),
+      //       Text('A', style: TextStyle(fontSize: 25),),
+      //       Text('A', style: TextStyle(fontSize: 25),),
+      //       Text('A', style: TextStyle(fontSize: 25),),
+      //       Text('A', style: TextStyle(fontSize: 25),),
+      //       Text('A', style: TextStyle(fontSize: 25),),
+      //       ElevatedButton(onPressed: (){}, child: Text('Click')),
+      //     ],
+      //   ),
+      // ),
+
+
+      // body: Center(
+      //   child: Container(
+      //     width: 100,
+      //     height: 100,
+      //     child: Image.asset('assets/images/Saiyam_Verma.jpg'),
+      //   ),
+      // )
+
+
+      // body: OutlinedButton(
+      //   child: Text("Click me!"),
+      //   onPressed: (){
+      //     print("Elevated Button Tapped");
+      //   },
+      //   onLongPress: (){
+      //     print("Long pressed");
+      //   },
+      // ),
+
+      // body: Center(
+      //   child: Container(
+      //     width: 100,
+      //     height: 100,
+      //     color: Colors.orange,
+      //     child: Center(
+      //       child: Text("Hello ji", style: TextStyle(
+      //         fontSize: 25,
+      //         color: Colors.pink,
+      //         fontWeight: FontWeight.w800,
+      //         backgroundColor: Colors.black54,
+      //       ),),
+      //
+      //     ),
+      //   ),
+      // )
     );
   }
 }
